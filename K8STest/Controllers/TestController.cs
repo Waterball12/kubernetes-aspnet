189038eb3d89eb73bbda64055db13d027fb55c2a
Waterball <waterballs12@gmail.com>
2020-12-13 23:25:26 +0000

    Initial commit

---
+++ K8STest/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace K8STest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Task<IActionResult> Get()
+        {
+            _logger.LogInformation("Successfully received request!");
+            return Task.FromResult((IActionResult)Ok("Test success!"));
+        }
+    }
+}
